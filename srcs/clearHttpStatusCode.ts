2e5f3b20e45b7c9c8cd13b35e6d46648585701d4
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-07-13 02:15:29 +0900

    feat(clearhttpstatuscode.ts): implement clearHttpStatusCode.ts

---
+++ srcs/clearHttpStatusCode.ts
@@ -0,0 +1,10 @@
+function clearHttpStatusCode() {
+	try {
+		const SPREADSHEET_ID: string = PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID');
+		const MAIN_SHEET: Sheet = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName('Main');
+		const LAST_ROW = MAIN_SHEET.getLastRow();
+		MAIN_SHEET.getRange(`N3:O${LAST_ROW}`).clearContent();
+	} catch (e) {
+		console.log(e.message);
+	}
+}
