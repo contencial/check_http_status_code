06f5fef2e5aaa0808f7f3f02c77c10ed4bda3f34
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-07-13 17:10:44 +0900

    fix(checkhttpstatuscode.ts): add new function: getIndexOfTargetCell
    - HTTPステータスコードが空白でないドメインをスキップする処理を追加

--- srcs/checkHttpStatusCode.ts
+++ srcs/checkHttpStatusCode.ts
@@ -4,22 +4,15 @@ function checkHttpStatusCode() {
 	const SPREADSHEET_ID = PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID');
 	const MAIN_SHEET = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName('Main');
 	const SIZE_CELL = MAIN_SHEET.getRange('B1');
-	const LIST_SIZE = SIZE_CELL.getValue();
+	const LIST_SIZE: int = SIZE_CELL.getValue();
 	try {
 		if (SIZE_CELL.isBlank() || typeof LIST_SIZE != 'number' || LIST_SIZE < 0)
 			throw new Error('List Size Error: 取得数に誤りがあります。');
-		const HTTP_CELL = MAIN_SHEET.getRange(3, 14, LIST_SIZE, 1).getValues();
+		const HTTP_CELL = MAIN_SHEET.getRange(3, 13, LIST_SIZE, 1).getValues();
 		let index: int = 0;
-		for (let row = 3; row < 3 + LIST_SIZE; row++) {
-			if (HTTP_CELL[index][0]) {
-				index++;
-			} else {
-				console.log(`Start at index: ${index}`);
-				break ;
-			}
-		}
+		index = getIndexOfTargetCell(HTTP_CELL, LIST_SIZE, index);
 		const DOMAIN_CELL = MAIN_SHEET.getRange(3, 5, LIST_SIZE, 1).getValues();
-		for (let row = 3 + index; row < 3 + LIST_SIZE; row++) {
+		while (index < LIST_SIZE) {
 			const URL = `https://${DOMAIN_CELL[index][0]}`;
 			let options = {
 				muteHttpExceptions: true
@@ -32,11 +25,14 @@ function checkHttpStatusCode() {
 				err = e.message.split(':');
 				value = err[0];
 			}
-			MAIN_SHEET.getRange(3 + index, 14).setValue(value);
+			MAIN_SHEET.getRange(3 + index, 13).setValue(value);
 			const NOW = new Date();
 			const TIMESTAMP = `${NOW.getFullYear()}/${NOW.getMonth()+1}/${NOW.getDate()} ${NOW.getHours()}:${NOW.getMinutes()}:${NOW.getSeconds()}`;
-			MAIN_SHEET.getRange(3 + index, 15).setValue(TIMESTAMP);
+			MAIN_SHEET.getRange(3 + index, 14).setValue(TIMESTAMP);
 			index++;
+			if (HTTP_CELL[index][0]) {
+				index = getIndexOfTargetCell(HTTP_CELL, LIST_SIZE, index);
+			}
 
 			let diff = NOW.getTime() - START.getTime();
 			if (diff / 1000 > 270)
@@ -45,4 +41,16 @@ function checkHttpStatusCode() {
 	} catch (e) {
 		console.log(e.message);
 	}
+}
+
+function getIndexOfTargetCell(HTTP_CELL: Array<Array<string>>, LIST_SIZE: int, index: int) {
+	while (index < LIST_SIZE) {
+		if (HTTP_CELL[index][0]) {
+			index++;
+		} else {
+			console.log(`Start at index: ${index}`);
+			break ;
+		}
+	}
+	return index;
 }
