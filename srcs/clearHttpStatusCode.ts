85b0205561fae071a3a43b9c74af2b6224046f96
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-07-23 17:43:53 +0900

    fix(clearhttpstatuscode.ts): modify parameter of getRange to I:J

--- srcs/clearHttpStatusCode.ts
+++ srcs/clearHttpStatusCode.ts
@@ -3,7 +3,7 @@ function clearHttpStatusCode() {
 		const SPREADSHEET_ID: string = PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID');
 		const MAIN_SHEET: Sheet = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName('Main');
 		const LAST_ROW = MAIN_SHEET.getLastRow();
-		MAIN_SHEET.getRange(`I3:O${LAST_ROW}`).clearContent();
+		MAIN_SHEET.getRange(`I3:J${LAST_ROW}`).clearContent();
 	} catch (e) {
 		console.log(e.message);
 	}
