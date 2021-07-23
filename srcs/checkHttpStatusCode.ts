6408ee3cfda2cafe3ea449cb255ba650dabd108b
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-07-23 15:16:08 +0900

    fix(checkhttpstatuscode.ts): move http status code column: 14 to 9

--- srcs/checkHttpStatusCode.ts
+++ srcs/checkHttpStatusCode.ts
@@ -8,12 +8,16 @@ function checkHttpStatusCode() {
 	try {
 		if (SIZE_CELL.isBlank() || typeof LIST_SIZE != 'number' || LIST_SIZE < 0)
 			throw new Error('List Size Error: 取得数に誤りがあります。');
-		const HTTP_CELL = MAIN_SHEET.getRange(3, 14, LIST_SIZE, 1).getValues();
-		let index: int = 0;
-		index = getIndexOfTargetCell(HTTP_CELL, LIST_SIZE, index);
-		const DOMAIN_CELL = MAIN_SHEET.getRange(3, 5, LIST_SIZE, 1).getValues();
-		while (index < LIST_SIZE) {
-			const URL = `https://${DOMAIN_CELL[index][0]}`;
+		const DOMAIN_INFO = MAIN_SHEET.getRange(3, 5, LIST_SIZE, 5).getValues();
+		DOMAIN_INFO.forEach(function(data, index) {
+			if (data[4]) {
+				return ;
+			}
+			if (data[1] == false || data[2] == false || data[3] == false) {
+				MAIN_SHEET.getRange(3 + index, 9).setValue('FALSE');
+				return ;
+			}
+			const URL = `https://${data[0]}`;
 			let options = {
 				muteHttpExceptions: true
 			};
@@ -25,32 +29,16 @@ function checkHttpStatusCode() {
 				err = e.message.split(':');
 				value = err[0];
 			}
-			MAIN_SHEET.getRange(3 + index, 14).setValue(value);
+			MAIN_SHEET.getRange(3 + index, 9).setValue(value);
 			const NOW = new Date();
 			const TIMESTAMP = `${NOW.getFullYear()}/${NOW.getMonth()+1}/${NOW.getDate()} ${NOW.getHours()}:${NOW.getMinutes()}:${NOW.getSeconds()}`;
-			MAIN_SHEET.getRange(3 + index, 15).setValue(TIMESTAMP);
-			index++;
-			if (HTTP_CELL[index][0]) {
-				index = getIndexOfTargetCell(HTTP_CELL, LIST_SIZE, index);
-			}
+			MAIN_SHEET.getRange(3 + index, 10).setValue(TIMESTAMP);
 
 			let diff = NOW.getTime() - START.getTime();
 			if (diff / 1000 > 270)
-				break ;
-		}
+				throw new Error('checkHttpStatusCode: Timeout 5 min') ;
+		});
 	} catch (e) {
 		console.log(e.message);
 	}
-}
-
-function getIndexOfTargetCell(HTTP_CELL: Array<Array<string>>, LIST_SIZE: int, index: int) {
-	while (index < LIST_SIZE) {
-		if (HTTP_CELL[index][0]) {
-			index++;
-		} else {
-			console.log(`Start at index: ${index}`);
-			break ;
-		}
-	}
-	return index;
 }
