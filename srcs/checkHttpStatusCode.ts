0356cdb8af94eff7aa0bf3025b0d118324e96e2e
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-07-26 00:11:25 +0900

    fix(checkhttpstatuscode.ts): modify the timing when to check timeout

--- srcs/checkHttpStatusCode.ts
+++ srcs/checkHttpStatusCode.ts
@@ -5,16 +5,27 @@ function checkHttpStatusCode() {
 	const MAIN_SHEET = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName('Main');
 	const SIZE_CELL = MAIN_SHEET.getRange('B1');
 	const LIST_SIZE: int = SIZE_CELL.getValue();
+	console.log('Start checkHttpStatusCode');
 	try {
 		if (SIZE_CELL.isBlank() || typeof LIST_SIZE != 'number' || LIST_SIZE < 0)
 			throw new Error('List Size Error: 取得数に誤りがあります。');
 		const DOMAIN_INFO = MAIN_SHEET.getRange(3, 5, LIST_SIZE, 5).getValues();
+		let i: number;
 		DOMAIN_INFO.forEach(function(data, index) {
+			i = index;
 			if (data[4]) {
 				return ;
 			}
+
+			const NOW = new Date();
+			const TIMESTAMP = `${NOW.getFullYear()}/${NOW.getMonth()+1}/${NOW.getDate()} ${NOW.getHours()}:${NOW.getMinutes()}:${NOW.getSeconds()}`;
+			let diff = NOW.getTime() - START.getTime();
+			if (diff / 1000 > 240)
+				throw new Error('checkHttpStatusCode: Timeout 5 min') ;
+
 			if (data[1] == false || data[2] == false || data[3] == false) {
 				MAIN_SHEET.getRange(3 + index, 9).setValue('-');
+				MAIN_SHEET.getRange(3 + index, 10).setValue(TIMESTAMP);
 				return ;
 			}
 			const URL = `https://${data[0]}`;
@@ -30,15 +41,11 @@ function checkHttpStatusCode() {
 				value = err[0];
 			}
 			MAIN_SHEET.getRange(3 + index, 9).setValue(value);
-			const NOW = new Date();
-			const TIMESTAMP = `${NOW.getFullYear()}/${NOW.getMonth()+1}/${NOW.getDate()} ${NOW.getHours()}:${NOW.getMinutes()}:${NOW.getSeconds()}`;
 			MAIN_SHEET.getRange(3 + index, 10).setValue(TIMESTAMP);
-
-			let diff = NOW.getTime() - START.getTime();
-			if (diff / 1000 > 270)
-				throw new Error('checkHttpStatusCode: Timeout 5 min') ;
 		});
 	} catch (e) {
-		console.log(e.message);
+		console.log(`FINISH: index: ${i}: ${e.message}`);
+	} finally {
+		console.log(`FINISH: index: ${i}`);
 	}
 }
