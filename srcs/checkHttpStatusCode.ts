41bc0e9fc3fe35cb0964a2941e0d51a0ffb10572
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-08-06 16:07:59 +0900

    fix: modify the order of column DOMAIN

--- srcs/checkHttpStatusCode.ts
+++ srcs/checkHttpStatusCode.ts
@@ -5,11 +5,12 @@ function checkHttpStatusCode() {
 	const MAIN_SHEET = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName('Main');
 	const SIZE_CELL = MAIN_SHEET.getRange('B1');
 	const LIST_SIZE: int = SIZE_CELL.getValue();
+	const DOMAIN_COL: int = 6;
 	console.log('Start checkHttpStatusCode');
 	try {
 		if (SIZE_CELL.isBlank() || typeof LIST_SIZE != 'number' || LIST_SIZE < 0)
 			throw new Error('List Size Error: 取得数に誤りがあります。');
-		const DOMAIN_INFO = MAIN_SHEET.getRange(3, 5, LIST_SIZE, 5).getValues();
+		const DOMAIN_INFO = MAIN_SHEET.getRange(3, DOMAIN_COL, LIST_SIZE, 5).getValues();
 		let i: number;
 		DOMAIN_INFO.forEach(function(data, index) {
 			i = index;
@@ -24,8 +25,8 @@ function checkHttpStatusCode() {
 				throw new Error('checkHttpStatusCode: Timeout 5 min') ;
 
 			if (data[1] == false || data[2] == false || data[3] == false) {
-				MAIN_SHEET.getRange(3 + index, 9).setValue('-');
-				MAIN_SHEET.getRange(3 + index, 10).setValue(TIMESTAMP);
+				MAIN_SHEET.getRange(3 + index, DOMAIN_COL + 4).setValue('-');
+				MAIN_SHEET.getRange(3 + index, DOMAIN_COL + 5).setValue(TIMESTAMP);
 				return ;
 			}
 			const URL = `https://${data[0]}`;
@@ -40,8 +41,8 @@ function checkHttpStatusCode() {
 				err = e.message.split(':');
 				value = err[0];
 			}
-			MAIN_SHEET.getRange(3 + index, 9).setValue(value);
-			MAIN_SHEET.getRange(3 + index, 10).setValue(TIMESTAMP);
+			MAIN_SHEET.getRange(3 + index, DOMAIN_COL + 4).setValue(value);
+			MAIN_SHEET.getRange(3 + index, DOMAIN_COL + 5).setValue(TIMESTAMP);
 		});
 	} catch (e) {
 		console.log(`FINISH: index: ${i}: ${e.message}`);
